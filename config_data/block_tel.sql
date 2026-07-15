e99e0dff6632c6dfe229cf000da0d8abd94c1cc0
abitsiflow <xtiangzmen@gmail.com>
2026-07-15 15:10:51 +0800

    UPDATE: added sql script

---
+++ config_data/block_tel.sql
@@ -0,0 +1,17 @@
+drop database if exists guests_Booked;
+
+create database guests_Booked;
+
+use guests_Booked;
+
+create table guests_status(
+	guest_id int auto_increment primary key,
+	guest_fullname CHAR(50),
+    guest_email CHAR(50),
+    guest_roomType CHAR(50),
+    guest_specialRequests CHAR(100),
+    guest_checkin datetime, 
+    guest_checkout date
+);
+
+

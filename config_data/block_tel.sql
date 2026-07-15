cae3a1ad3024f7da6c5eb6b8d969260b666677fc
abitsiflow <xtiangzmen@gmail.com>
2026-07-15 15:13:41 +0800

    UPDATE: adjusted data type on sql script

--- config_data/block_tel.sql
+++ config_data/block_tel.sql
@@ -7,7 +7,7 @@ use guests_Booked;
 create table guests_status(
 	guest_id int auto_increment primary key,
 	guest_fullname CHAR(50),
-    guest_email CHAR(50),
+    guest_email VARCHAR(50),
     guest_roomType CHAR(50),
     guest_specialRequests CHAR(100),
     guest_checkin datetime, 
